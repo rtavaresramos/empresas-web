ef195758a4b343dedd4c4131cc7ddd6325937240
Rodrigo Ramos <rodrigoramos@MacBook-Pro-de-Rodrigo.local>
2021-01-17 20:53:18 -0300

    Creating project intern Routes

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Company from "./pages/Company";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/company" exact component={Company} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
